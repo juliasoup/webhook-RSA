1639ecc61fc92f1a2c5b5534f226ec554e5b7505
Julia Soupinski Rodrigues <164052139+juliasoup@users.noreply.github.com>
2025-06-03 20:08:30 -0300

    main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from tkinter import simpledialog
 from interface import ChatApp
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    root = tk.Tk() #cria a janela tkinter
     root.withdraw()
     role = simpledialog.askstring("Papel", "Digite 's' para servidor ou 'c' para cliente:").strip().lower()
-    root.deiconify()
+    root.deiconify() #mostra a janela principal do chat
     is_server = role == 's'
     app = ChatApp(root, is_server)
     root.mainloop()
